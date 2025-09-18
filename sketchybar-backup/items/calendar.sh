ca368f307c78d40ab02cff23fbbc46f69c355a6e
OY! Giovanno Rachmat <giovanno.rachmat@oyindonesia.com>
2025-09-18 15:49:44 +0700

    remove backup and CTT's config

--- sketchybar-backup/items/calendar.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sketchybar --add item $NAME right \
-           --set $NAME icon=􀉉  script="$PLUGIN_DIR/calendar.sh"
--- sketchybar-titus/plugins/calendar.sh
+++
@@ -1,2 +0,0 @@
-# Example of updating the clock format
-sketchybar --set "$NAME" label="$(date '+%a %d %b %I:%M %p')"
