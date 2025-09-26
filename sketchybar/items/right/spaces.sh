018c05eabd7f1b446557dba4f56099dda5866810
OY! Giovanno Rachmat <giovanno.rachmat@oyindonesia.com>
2025-09-26 17:05:58 +0700

    removed folders

--- sketchybar/items/right/spaces.sh
+++
@@ -1,7 +0,0 @@
-#!bin/sh
-
-sketchybar --add bracket spaces '/space\..*/' \
-           --set spaces background.color=$TRANSPARENT \
-                        background.corner_radius=5 \
-                        background.border_width=1 \
-                        background.height=25
