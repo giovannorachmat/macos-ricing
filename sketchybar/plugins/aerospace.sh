732ff4acc8009080649127ca3d6c4e8c09a9bce2
OY! Giovanno Rachmat <giovanno.rachmat@oyindonesia.com>
2025-09-27 19:10:06 +0700

    updated aerospace

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -2,16 +2,20 @@
 
 FOCUSED_WORKSPACE=$(aerospace list-workspaces --focused)
 
-# sketchybar --set space label="$FOCUSED_WORKSPACE"
+if [ "$SENDER" = "aerospace_service_mode_enabled_changed" ]; then
+  if [ "$AEROSPACE_SERVICE_MODE_ENABLED" = true ]; then
+    sketchybar --set workspaces_service_mode_indicator \
+      label.drawing=on
+  else
+    sketchybar --set workspaces_service_mode_indicator \
+      label.drawing=off
+  fi
+fi
 
-# if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-#   sketchybar --set $NAME background.drawing=on space label="$FOCUSED_WORKSPACE"
-# else
-#   sketchybar --set $NAME background.drawing=off space label="$FOCUSED_WORKSPACE"
-# fi
-
-if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
+if [ "$SENDER" = "aerospace_workspace_change" ]; then
+  if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
     sketchybar --set $NAME background.drawing=on
-else
+  else
     sketchybar --set $NAME background.drawing=off
+  fi
 fi
