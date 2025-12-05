9a1e2ba46b77e17b6a90978e7d560856a7025a1c
OY! Giovanno Rachmat <giovanno.rachmat@oyindonesia.com>
2025-12-05 17:54:36 +0700

    updated brew outdated and wifi sketchybar app

--- sketchybar/plugins/brew_outdated.sh
+++ sketchybar/plugins/brew_outdated.sh
@@ -4,7 +4,6 @@ BREW_OUTPUT=$(brew outdated --greedy)
 COUNT=$(echo "$BREW_OUTPUT" | wc -l | tr -d ' ')
 
 # debugging
-# echo "Brew output: '$BREW_OUTPUT'" >>/tmp/brew_debug.log
-# echo "Count: $COUNT" >>/tmp/brew_debug.log
+echo "Brew output: '$BREW_OUTPUT'" >>/private/tmp/brew_debug.log
 
-sketchybar --set "$NAME" label="$COUNT"
+sketchybar --set "$NAME" label="$BREW_OUTPUT"
