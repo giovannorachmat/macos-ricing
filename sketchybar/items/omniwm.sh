8b83cbd7fe00ad45353f82405e1930ce3864bdb7
Giovanno Rachmat <giovannorachmat@Giovannos-MacBook-Pro.local>
2026-08-01 12:07:00 +0700

    updated sketchybar to place omniwm workspace changes

--- sketchybar/items/omniwm.sh
+++ sketchybar/items/omniwm.sh
@@ -1,43 +1,37 @@
 #!/usr/bin/env bash
 
-sketchybar --add event omniwm_workspace_change
+FOCUSED="$(
+  omniwmctl query active-workspace |
+    jq -r '.result.payload.workspace.displayName'
+)"
 
-FOCUSED_WORKSPACE=$(omniwmctl subscribe active-workspace --no-send-initial | jq -r '.result.payload.workspace.number // empty')
-
-space=(
-  icon="$FOCUSED_WORKSPACE"
-  icon.color=$WHITE
-  icon.padding_left=12
-  background.color=$BACKGROUND_2
-  background.border_color=$MAGENTA
-  background.border_width=1
-  background.corner_radius=5
-  background.height=20
-  background.drawing=on
-)
+[ -z "$FOCUSED" ] && FOCUSED="-"
 
 sketchybar \
-  --add item space left \
-  --subscribe space omniwm_workspace_change \
-  --set space "${space[@]}"
+  --add item omni_space left \
+  --set omni_space \
+  icon="$FOCUSED" \
+  icon.color=$WHITE \
+  icon.padding_left=12 \
+  background.color=$BACKGROUND_2 \
+  background.border_color=$MAGENTA \
+  background.border_width=1 \
+  background.corner_radius=5 \
+  background.height=20 \
+  background.drawing=on
 
-#sketchybar --add event omniwm_workspace_change
+# space=(
+#   icon="$FOCUSED"
+#   icon.color=$WHITE
+#   icon.padding_left=12
+#   background.color=$BACKGROUND_2
+#   background.border_color=$MAGENTA
+#   background.border_width=1
+#   background.corner_radius=5
+#   background.height=20
+#   background.drawing=on
+# )
 #
-#FOCUSED=$(omniwmctl query active-workspace | jq -r '.result.payload.workspace.number // empty')
-#
-#space=(
-#  icon="$FOCUSED"
-#  icon.color=$WHITE
-#  icon.padding_left=12
-#  background.color=$BACKGROUND_2
-#  background.border_color=$MAGENTA
-#  background.border_width=1
-#  background.corner_radius=5
-#  background.height=20
-#  background.drawing=on
-#)
-#
-#sketchybar \
-#  --add item space left \
-#  --subscribe space omniwm_workspace_change \
-#  --set space "${space[@]}"
+# sketchybar \
+#   --add item space left \
+#   --set space "${space[@]}"
