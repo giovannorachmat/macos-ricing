e02eab78aed42a1b4a6a300a1a49c284151b43f7
Giovanno Rachmat <giovannorachmat@Giovannos-MacBook-Pro.local>
2026-08-01 22:10:52 +0700

    ditched omniwm and revert back to sketchybar

--- sketchybar/items/aerospace.sh
+++ sketchybar/items/aerospace.sh
@@ -1,30 +1,21 @@
 #!/usr/bin/env bash
 
-sketchybar --add event aerospace_workspace_change
+FOCUSED="$(aerospace list-workspaces --focused)"
 
-FOCUSED=$(aerospace list-workspaces --focused)
-
-if [ -z "$FOCUSED" ]; then
-  FOCUSED="No Window"
-fi
-
-space=(
-  icon="$FOCUSED"
-  icon.color=$WHITE
-  icon.padding_left=12
-  background.color=$BACKGROUND_2
-  background.border_color=$MAGENTA
-  background.border_width=1
-  background.corner_radius=5
-  background.height=20
-  background.drawing=on
-  script="$PLUGIN_DIR/aerospace.sh $FOCUSED"
-)
+[ -z "$FOCUSED" ] && FOCUSED="-"
 
 sketchybar \
-  --add item space left \
-  --subscribe space aerospace_workspace_change \
-  --set space "${space[@]}"
+  --add item aero_space left \
+  --set aero_space \
+  icon="$FOCUSED" \
+  icon.color=$WHITE \
+  icon.padding_left=12 \
+  background.color=$BACKGROUND_2 \
+  background.border_color=$MAGENTA \
+  background.border_width=1 \
+  background.corner_radius=5 \
+  background.height=20 \
+  background.drawing=on
 
 # for sid in $(aerospace list-workspaces --all); do
 #   space=(
