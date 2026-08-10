7e9c67877c2ca2daf0b866374253d1b061d7723c
Giovanno Rachmat <giovannorachmat@Giovannos-MacBook-Pro.local>
2026-08-10 12:16:35 +0700

    updated labels for VPN and yabai in sketchybar

--- sketchybar/plugins/vpn.sh
+++ sketchybar/plugins/vpn.sh
@@ -5,7 +5,7 @@ source "$CONFIG_DIR/icons.sh"
 VPN=$(scutil --nc list | rg '\(Connected\)' | rg -o '"[^"]+"' | tr -d '"')
 
 if [[ -z "$VPN" ]]; then
-  sketchybar --set "$NAME" icon="VPN: " label="off"
+  sketchybar --set "$NAME" icon="VPN:" label="off"
 else
-  sketchybar --set "$NAME" icon="VPN: " label="$VPN"
+  sketchybar --set "$NAME" icon="VPN:" label="$VPN"
 fi
--- sketchybar/plugins/yabai.sh
+++ sketchybar/plugins/yabai.sh
@@ -2,7 +2,7 @@
 
 SPACE="$(yabai -m query --spaces | jq -r '.[] | select(.["has-focus"] == true) | .label')"
 DISPLAY="$(yabai -m query --spaces | jq -r '.[] | select(.["has-focus"] == true) | .display')"
-FOCUSED="$SPACE | $DISPLAY"
+FOCUSED="$SPACE-$DISPLAY"
 
 [ -z "$FOCUSED" ] && exit 0
 
